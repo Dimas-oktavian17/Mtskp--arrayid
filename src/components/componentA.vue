bba2b4b6305b11f3b05dee05cf56e45d64a47bee
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2022-12-27 13:59:56 +0700

    done create component registration vue in local

---
+++ src/components/componentA.vue
@@ -0,0 +1,9 @@
+<template>
+  <main
+    class="flex flex-col items-center justify-center w-full h-full bg-slate-500"
+  >
+    <div class="w-[50%] h-[50%]">
+      <h1 class="text-5xl text-white">THIS LOCAL COMPONENT REGISTRATION</h1>
+    </div>
+  </main>
+</template>
