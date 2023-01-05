1202b8b6b92eaf8fe16222653563daa2b1bca041
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2023-01-05 09:34:41 +0700

    done create reasons section

---
+++ src/components/component/bgtwo.vue
@@ -0,0 +1,5 @@
+<template>
+  <div
+    class="bg-[url('https://dimas-oktavian17.github.io/assets/iconFigma/Rectangletwo.svg')] absolute w-full h-[44.938rem] bg-cover top-[201.589rem]"
+  ></div>
+</template>
