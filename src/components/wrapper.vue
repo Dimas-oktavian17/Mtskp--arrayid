381d3196f0c6ec128997fcc40dd570c41b2e8439
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2023-01-09 14:36:58 +0700

    done create footer

--- src/components/wrapper.vue
+++ src/components/wrapper.vue
@@ -202,6 +202,223 @@ import Bgtwo from "./component/bgtwo.vue";
         </div>
       </div>
       <!-- end expresion -->
+      <!-- footer section -->
+      <div
+        class="container flex flex-col items-center mt-20 p-20 gap-[30px] bg-white rounded-tl-3xl rounded-tr-3xl"
+      >
+        <div class="flex flex-col items-start gap-[30px] lg:flex-row">
+          <div class="flex flex-col items-start gap-5 lg:w-[27rem] lg:gap-2">
+            <div class="flex flex-row items-center gap-[10px]">
+              <svg
+                width="50"
+                height="50"
+                viewBox="0 0 50 50"
+                fill="black"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  width="50"
+                  height="50"
+                  rx="25"
+                  fill="black"
+                  fill-opacity="0.08"
+                />
+                <path
+                  d="M15.76 29.106L19.792 25.038L15.76 20.988H18.658L22.69 25.038L18.658 29.106H15.76ZM24.9745 31.626C24.5545 31.626 24.2065 31.494 23.9305 31.23C23.6665 30.966 23.5345 30.648 23.5345 30.276C23.5345 29.892 23.6665 29.568 23.9305 29.304C24.2065 29.04 24.5545 28.908 24.9745 28.908C25.3945 28.908 25.7365 29.04 26.0005 29.304C26.2765 29.568 26.4145 29.892 26.4145 30.276C26.4145 30.648 26.2765 30.966 26.0005 31.23C25.7365 31.494 25.3945 31.626 24.9745 31.626ZM31.2684 29.106L27.2544 25.038L31.2684 20.988H34.1844L30.1344 25.038L34.1844 29.106H31.2684Z"
+                  fill="black"
+                />
+              </svg>
+              <h1
+                class="text-3xl order-1 text-left font-bold font-header text-homePrimary"
+              >
+                Expression
+              </h1>
+            </div>
+            <p class="order-1 text-lg text-homePrimary font-normal font-header">
+              Sebenernya banyak banget aplikasi chatting yang lebih bagus dari
+              ini, maka dari itu lebih baik pake yang lain aja deh ya daripada
+              pake aplikasi yang ini.
+            </p>
+            <p
+              class="order-2 text-sm font-semibold tracking-widest font-header text-homePrimary"
+            >
+              COPYRIGHT (C) 2021. DESIGN BY NAUVAL
+            </p>
+            <p
+              class="order-2 text-sm font-semibold tracking-widest font-header text-homePrimary"
+            >
+              COPYRIGHT (C) 2022. CREATE BY OKTA
+            </p>
+          </div>
+          <div
+            class="flex flex-col items-start flex-grow order-1 gap-4 lg:gap-2"
+          >
+            <h1 class="text-lg font-bold text-homePrimary font-header">
+              Sitemap
+            </h1>
+            <div class="flex flex-col items-start gap-[10px] order-1">
+              <ul class="flex flex-col gap-4">
+                <li class="text-base font-normal font-header text-homePrimary">
+                  <a href="#beranda">Beranda</a>
+                </li>
+                <li class="text-base font-normal font-header text-homePrimary">
+                  <a href="#fitur">Fitur-fitur</a>
+                </li>
+                <li class="text-base font-normal font-header text-homePrimary">
+                  <a href="#teknologi">Teknologi</a>
+                </li>
+                <li class="text-base font-normal font-header text-homePrimary">
+                  <a href="#about">About</a>
+                </li>
+                <li class="text-base font-normal font-header text-homePrimary">
+                  <a href="#faq">FAQ</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div
+            class="flex flex-col items-start flex-grow order-2 gap-5 lg:gap-2"
+          >
+            <h1 class="text-lg font-bold text-homePrimary font-header">
+              Tetep Terhubung
+            </h1>
+            <p
+              class="self-stretch order-1 text-base font-normal font-header text-homePrimary"
+            >
+              Lihat kami pansos di sosial media berikut:
+            </p>
+            <div
+              class="flex flex-row justify-center items-center gap-[10px] order-2"
+            >
+              <div
+                class="group flex flex-row justify-center items-center p-2 gap-[10px] w-10 h-10 bg-bg-icon rounded-md cursor-pointer transition-all duration-1000"
+              >
+                <a
+                  href="https://www.instagram.com/dimas_okr/?hl=id"
+                  target="_blank"
+                >
+                  <svg
+                    class="group-hover:fill-white"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M17 2H7C4.23858 2 2 4.23858 2 7V17C2 19.7614 4.23858 22 7 22H17C19.7614 22 22 19.7614 22 17V7C22 4.23858 19.7614 2 17 2Z"
+                      stroke="#0C1B4D"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M16 11.3698C16.1234 12.2021 15.9813 13.052 15.5938 13.7988C15.2063 14.5456 14.5932 15.1512 13.8416 15.5295C13.0901 15.9077 12.2385 16.0394 11.4078 15.9057C10.5771 15.7721 9.80977 15.3799 9.21485 14.785C8.61993 14.1901 8.22774 13.4227 8.09408 12.592C7.96042 11.7614 8.09208 10.9097 8.47034 10.1582C8.8486 9.40667 9.4542 8.79355 10.201 8.40605C10.9478 8.01856 11.7978 7.8764 12.63 7.99981C13.4789 8.1257 14.2649 8.52128 14.8717 9.12812C15.4785 9.73496 15.8741 10.5209 16 11.3698Z"
+                      stroke="#0C1B4D"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M17.5 6.5H17.51"
+                      stroke="#0C1B4D"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </a>
+              </div>
+              <div
+                class="group flex flex-row justify-center items-center p-2 gap-[10px] w-10 h-10 bg-bg-icon rounded-md transition-all duration-1000 cursor-pointer"
+              >
+                <a
+                  href="https://www.facebook.com/dimas.oktavian.5055"
+                  target="_blank"
+                >
+                  <svg
+                    class="group-hover:fill-white"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 31"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g filter="url(#filter0_d_27_427)">
+                      <path
+                        d="M18 2H15C13.6739 2 12.4021 2.52678 11.4645 3.46447C10.5268 4.40215 10 5.67392 10 7V10H7V14H10V22H14V14H17L18 10H14V7C14 6.73478 14.1054 6.48043 14.2929 6.29289C14.4804 6.10536 14.7348 6 15 6H18V2Z"
+                        stroke="#0C1B4D"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </g>
+                    <defs>
+                      <filter
+                        id="filter0_d_27_427"
+                        x="-4"
+                        y="0"
+                        width="32"
+                        height="32"
+                        filterUnits="userSpaceOnUse"
+                        color-interpolation-filters="sRGB"
+                      >
+                        <feFlood
+                          flood-opacity="0"
+                          result="BackgroundImageFix"
+                        />
+                        <feColorMatrix
+                          in="SourceAlpha"
+                          type="matrix"
+                          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                          result="hardAlpha"
+                        />
+                        <feOffset dy="4" />
+                        <feGaussianBlur stdDeviation="2" />
+                        <feComposite in2="hardAlpha" operator="out" />
+                        <feColorMatrix
+                          type="matrix"
+                          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+                        />
+                        <feBlend
+                          mode="normal"
+                          in2="BackgroundImageFix"
+                          result="effect1_dropShadow_27_427"
+                        />
+                        <feBlend
+                          mode="normal"
+                          in="SourceGraphic"
+                          in2="effect1_dropShadow_27_427"
+                          result="shape"
+                        />
+                      </filter>
+                    </defs>
+                  </svg>
+                </a>
+              </div>
+              <div
+                class="group flex flex-row justify-center items-center p-2 gap-[10px] w-10 h-10 bg-bg-icon rounded-md transition-all duration-1000 cursor-pointer"
+              >
+                <a href="https://github.com/Dimas-oktavian17" target="_blank">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="currentColor"
+                    class="bi bi-github group-hover:fill-white"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- end footer -->
     </div>
     <!-- feature  -->
     <!-- 2380px -->
